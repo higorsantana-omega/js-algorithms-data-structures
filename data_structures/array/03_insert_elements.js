4faa242cee63c4503c1c13eeacf1280cc91aced4
Higor Santana <70420430+higorsantana-omega@users.noreply.github.com>
2022-02-11 19:16:09 -0300

    update

---
+++ data_structures/array/03_insert_elements.js
@@ -0,0 +1,15 @@
+const number = []
+
+// Insert element in a array
+// Arrays in javascript grow dynamically
+number[number.length] = 2 // [2]
+number[number.length] = 10
+console.log(number)
+
+// Insert elements in array with method push
+number.push(12) // [2, 10, 12]
+number.push(20, 30) // Insert many elements
+console.log(number)
+
+
+
