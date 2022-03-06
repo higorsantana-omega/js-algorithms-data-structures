2b5979d10db0c9a36fbd2009f0316fa0550d696b
Higor Santana <70420430+higorsantana-omega@users.noreply.github.com>
2022-03-05 21:06:37 -0300

    concat arrays

---
+++ data_structures/array/06_concat_arrays.js
@@ -0,0 +1,8 @@
+// to concat arrays use .concat
+const numberZero = 0
+const positiveNumbers = [1, 2, 3]
+const negativeNumbers = [-3, -2, -1]
+
+const numbers = negativeNumbers.concat(0, positiveNumbers)
+console.log(numbers)
+
